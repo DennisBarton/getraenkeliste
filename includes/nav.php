86c9b49e90350a00c2509fde10538196479775cd
db-archer <db-archer@test.com>
2025-12-20 04:15:06 +0100

    fastpush

--- includes/nav.php
+++ includes/nav.php
@@ -2,7 +2,6 @@
   <a href="abrechnung_anzeigen.php?date=today&correct=1" style="color:red; font-weight:bold">Korrektur!</a>
   <a href="abrechnung_anzeigen.php">Offene Zahlungen</a>
   <a href="abrechnung_anzeigen.php?paid=1">Bezahlte Einträge</a>
-  <a href="verkauf_eintragen.php">Verkauf eintragen</a>
 <!--
   <p><li><a href="index.php">Home</a></li></p>
   <li><a href="kunden.php">Kunden</a></li>
